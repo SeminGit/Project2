46d8a2c61699d54b143367ee5608fbc2d3147dea
SeminGit <57789204+SeminGit@users.noreply.github.com>
2020-05-19 11:15:20 +0300

    v2

--- Model/Constants.cs
+++ Model/Constants.cs
@@ -2,7 +2,7 @@
 {
     class Constants
     {
-        public readonly static string tweetPattern = @"\[[^\]]+\])(\s_\s[^\[]+",
+        public readonly static string tweetPattern = @"\[[^\]]+\]\s_\s[^\[]+",
                                      coordinatesPattern = @"\[[\d.,\s-]+\]",
                                      datePattern = @"\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}",
                                      textPattern = @"\[[^\]]+\]\s_\s\d{ 4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\s";
